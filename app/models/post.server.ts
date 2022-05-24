13cd9415fc95f8586436512922334d8563f62ae3
Fabio <lazzaroni@jeflab.it>
2022-05-25 00:16:24 +0200

    ✨: implemented create post

--- app/models/post.server.ts
+++ app/models/post.server.ts
@@ -1,6 +1,12 @@
 import {prisma} from "~/db.server";
+import type {Post} from "@prisma/client";
+export type {Post};
 
-export type {Post} from "@prisma/client";
+export async function createPost(
+  post: Pick<Post, "slug" | "title" | "markdown">
+) {
+  return prisma.post.create({data: post});
+}
 
 export async function getPosts() {
   return prisma.post.findMany();
