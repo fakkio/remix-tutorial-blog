ff1d6d2dbe3852f69c04b1d5114311028f00a271
Fabio <lazzaroni@jeflab.it>
2022-05-24 22:49:51 +0200

    🗃️: move and read data from db

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -1,27 +1,14 @@
 import {json} from "@remix-run/node";
 import {Link, useLoaderData} from "@remix-run/react";
-
-type Post = {
-  slug: string;
-  title: string;
-};
+import {getPosts} from "~/models/post.server";
 
 type LoaderData = {
-  posts: Array<Post>;
+  posts: Awaited<ReturnType<typeof getPosts>>;
 };
 
 export const loader = async () => {
   return json<LoaderData>({
-    posts: [
-      {
-        slug: "my-first-post",
-        title: "My First Post",
-      },
-      {
-        slug: "90s-mixtape",
-        title: "A Mixtape I Made Just For You",
-      },
-    ],
+    posts: await getPosts(),
   });
 };
 
